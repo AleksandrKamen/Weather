e254ada19b0dbe4011dc05d67f8f7a201bd03da1
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-01-13 15:33:47 +0300

    added HibernateUtil

---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+import lombok.experimental.UtilityClass;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+    private SessionFactory sessionFactory;
+    public Session getSession() {
+        return getSessionFactory().getCurrentSession();
+    }
+    private SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            var configuration = buildConfiguration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    private Configuration buildConfiguration() {
+        var configuration = new Configuration();
+        return configuration;
+    }
+}
